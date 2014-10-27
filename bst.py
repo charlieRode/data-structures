2c7ffac0aa88dbc42f319dfcdef241c70603a953
Charlie Rode <chrode10@gmail.com>
2014-10-26 19:27:02 -0700

    Add _search() method that traverses the tree in search of the specified value. If the value is found, return the Node to which it belongs. Otherwise, if the value is not found, return the position that the value should occupy (value == None) along with the parent Node that points to that position. **Except if the tree is empty, in which case, return None

--- bst.py
+++ bst.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 
 class Node(object):
-    def __init__(self, data, lchild=None, rchild=None):
+    def __init__(self, data):
         self.data = data
-        self.lchild = lchild
-        self.rchild = rchild
+        self.lchild = None
+        self.rchild = None
 
 class BST(object):
     def __init__(self):
-        self.head = None
-        
+        self.root = None
+
+    def _search(self, data):
+        if self.root is None:
+            return (None,)
+        trace = self.root
+        while trace is not None:
+            print trace.data
+            if trace.data == data:
+                return (trace,)
+            elif data < trace.data:
+                trace, _parent = trace.lchild, trace
+            else:
+                trace, _parent = trace.rchild, trace
+        return (trace, _parent)
+  
+
+    def insert(self, data):
+        pass
--- test_bst.py
+++ test_bst.py
@@ -1,7 +1,49 @@
 #!/usr/bin/env python
 
-from bst import BST
+from bst import BST, Node
 
 def test_init():
     tree = BST()
-    assert tree.head == None
+    assert tree.root == None
+
+def test_insert():
+    return
+    tree = BST()
+    tree.insert(0)
+    assert tree.root.data == 0
+    tree.insert(1)
+    assert tree.root.data == 1
+    assert tree.root.lchild == None
+    assert tree.root.rchild.data == 1
+    tree.insert(-1)
+    assert tree.root.lchild.data == -1
+
+def test_search():
+    tree = BST()
+    assert tree._search(0)[0] is None
+
+    tree.root = Node(0)
+    tree.root.lchild = Node(-2)
+    tree.root.rchild = Node(2)
+    tree.root.lchild.lchild = Node(-3)
+    tree.root.lchild.rchild = Node(-1)
+    tree.root.rchild.rchild = Node(3)
+    tree.root.rchild.lchild = Node(1)
+
+    assert tree._search(1)[0].data == 1
+    assert tree._search(-2)[0].data == -2
+    assert tree._search(4)[0] is None
+    assert tree._search(4)[1].data == 3
+
+
+def test_contains():
+    return
+    tree = BST()
+    tree.insert(0)
+    tree.insert(1)
+    tree.insert(-1)
+    tree.insert(2)
+    tree.insert(-2)
+    assert tree.contains(-1) == True
+    assert tree.contains(0) == True
+    assert tree.contains(4) == False
