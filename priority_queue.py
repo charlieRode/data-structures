fb2b05db3f869fcea19eb755993913c4684c977f
Charlie Rode <chrode10@gmail.com>
2014-10-30 18:17:25 -0700

    Add insert() functionality to queue. Add unit tests.

--- priority_queue.py
+++ priority_queue.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
-from mineap import Min_Heap
+from minheap import Min_Heap
 
 class PriorityQ(object):
     def __init__(self):
         self._heap = Min_Heap()
         self._vals = {}
 
-    #def insert(self, item, priority):
-        #self._vals[priority] = item
-        #self._heap.push(priority)
+    def insert(self, item, priority):
+        """Given an item and a priority, in that order, adds item to Q"""
+        if type(priority) is not int:
+            raise TypeError("priority must be type int")
+        else:
+            if priority in self._vals.keys():
+                self._vals[priority] += (item,)
+            else:
+                self._vals[priority] = (item,)
+                self._heap.push(priority)
 
     #def pop(self):
         #return self._vals[self._heap.pop()]
--- test_priority_queue.py
+++ test_priority_queue.py
@@ -5,3 +5,20 @@ from priority_queue import PriorityQ
 def test_init():
     p = PriorityQ()
     assert len(p._vals) == 0
+
+def test_insert():
+    p = PriorityQ()
+    p.insert("Tom Jones", 2)
+    p.insert(5, 1)
+    p.insert('X', 1)
+
+    try:
+        p.insert('stuff', 'a_string')
+    except TypeError:
+        assert True
+    else:
+        assert False
+
+    assert "Tom Jones" in p._vals[2]
+    assert 5 in p._vals[1]
+    assert 'X' in p._vals[1]
