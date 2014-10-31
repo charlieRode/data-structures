54de4e9db9cca22c074673aef674c7e4d166646f
Charlie Rode <chrode10@gmail.com>
2014-10-30 21:39:58 -0700

    Just updated some styel

--- queue.py
+++ queue.py
@@ -5,22 +5,22 @@ from linked_list import Linked_List
 class Queue:
 
     def __init__(self):
-        self._lst= Linked_List()
+        self._lst = Linked_List()
 
     def enqueue(self, datum):
         self._lst.insert(datum)
 
     def dequeue(self):
         if self._lst.size() > 1:
-            trace= self._lst.head
+            trace = self._lst.head
             while trace.pointer.pointer is not None:
-                trace= trace.pointer
-            result= trace.pointer.datum
-            trace.pointer= None
+                trace = trace.pointer
+            result = trace.pointer.datum
+            trace.pointer = None
             return result
-        elif self._lst.size()==1:
-            result= self._lst.head.datum
-            self._lst.head= None
+        elif self._lst.size() == 1:
+            result = self._lst.head.datum
+            self._lst.head = None
             return result
         else:
             raise ValueError("Cannot dequeue an empty queue")
