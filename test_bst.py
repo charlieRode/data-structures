799ed560d087e5a8b14d344d92acf3df541313e7
Charlie Rode <chrode10@gmail.com>
2014-11-01 16:09:31 -0700

    Add more test cases

--- test_bst.py
+++ test_bst.py
@@ -2,9 +2,11 @@
 
 from bst import BST, Node
 
+
 def test_init():
     tree = BST()
-    assert tree.root == None
+    assert tree.root is None
+
 
 def test_insert():
     tree = BST()
@@ -24,6 +26,7 @@ def test_insert():
     assert tree.root.rchild.data == 2
     assert tree.root.rchild.rchild.data == 3
     assert tree.root.rchild.lchild.data == 1
+
 
 def test_search():
     tree = BST()
@@ -50,9 +53,10 @@ def test_contains():
     tree.insert(-1)
     tree.insert(2)
     tree.insert(-2)
-    assert tree.contains(-1) == True
-    assert tree.contains(0) == True
-    assert tree.contains(4) == False
+    assert tree.contains(-1)
+    assert tree.contains(0)
+    assert tree.contains(4) is False
+
 
 def test_size():
     tree = BST()
@@ -67,6 +71,7 @@ def test_size():
     tree.insert(4)
     assert tree.size() == 6
 
+
 def test_depth():
     tree = BST()
     assert tree.depth() == 0
@@ -78,6 +83,7 @@ def test_depth():
     for item in items:
         tree.insert(item)
     assert tree.depth() == 5
+
 
 def test_balance():
     tree = BST()
@@ -91,16 +97,24 @@ def test_balance():
         tree.insert(item)
     assert tree.balance() == -1
 
+
 def test_delete():
     tree = BST()
-    for n in [0,2,-2,1,-1,3,-3]:
+    for n in [0, 2, -2, 1, -1, 3, -3]:
         tree.insert(n)
     for n in [-3, -2, -1, 0, 1, 2, 3]:
         assert tree.contains(n)
     tree.delete(-3)
-    assert tree.contains(-3) == False
+    assert tree.contains(-3) is False
     tree.delete(-2)
-    assert tree.contains(-2) == False
+    assert tree.contains(-2) is False
     for n in [-1, 0, 1, 2, 3]:
         assert tree.contains(n)
     assert tree.size() == 5
+    tree.insert(5)
+    tree.insert(4)
+    tree.insert(6)
+    tree.delete(3)
+    assert tree.size() == 7
+    for i in [-1, 0, 1, 2, 4, 5, 6]:
+        assert tree.contains(i)
