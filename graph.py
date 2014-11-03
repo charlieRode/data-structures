9907dfddb1655c427cee5e4746a9baa18588d437
Charlie Rode <chrode10@gmail.com>
2014-11-03 12:43:31 -0800

    Add init function and test case

--- graph.py
+++ graph.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python
+
+
+class Vertext(object):
+    def __init__(self, value):
+        self.value = value
+
+
+class Edge(object):
+    def __init__(self):
+        self.connects = None
+
+
+class Graph(object):
+    def __init__(self):
+        self.edges = []
+        self.verticies = []
--- test_graph.py
+++ test_graph.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python
+import pytest
+from graph import Edge, Vertext, Graph
+
+
+def test_init():
+    g = Graph()
+    assert g.edges == []
+    assert g.verticies == []
