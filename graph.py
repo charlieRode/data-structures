5b02e3775010f3a3f3b0211c78f556319ee4ac1a
Charlie Rode <chrode10@gmail.com>
2014-11-03 17:08:50 -0800

    Add add_edge)_, add_vertex methods and test cases. Delete Vertex class: verticies no longer distinguished from their data.

--- graph.py
+++ graph.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python
 
 
-class Vertext(object):
-    def __init__(self, value):
-        self.value = value
-
-
 class Edge(object):
-    def __init__(self):
-        self.connects = None
+    def __init__(self, vertex1, vertex2):
+        # For now, this is an unordered set since our edges have no direction
+        # and hence (v1, v2) will be counted the same as (v2, v1)
+        self.connects = set((vertex1, vertex2))
 
 
 class Graph(object):
     def __init__(self):
-        self.edges = []
-        self.verticies = []
+        self._edges = set()
+        self._verticies = set()
+
+    def nodes(self):
+        return self._verticies
+
+    def edges(self):
+        return [edge.connects for edge in self._edges]
+
+    def add_vertex(self, data):
+        self._verticies.add(data)
+        return
+
+    def add_edge(self, data1, data2):
+        self._verticies = self._verticies.union({data1, data2})
+        edge = Edge(data1, data2)
+        self._edges.add(edge)
+        return
+
--- test_graph.py
+++ test_graph.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
 import pytest
-from graph import Edge, Vertext, Graph
+from graph import Edge, Graph
 
 
 def test_init():
     g = Graph()
-    assert g.edges == []
-    assert g.verticies == []
+    assert len(g._edges) == 0
+    assert len(g._verticies) == 0
+
+
+def test_add_vertex():
+    g = Graph()
+    g.add_vertex(1)
+    g.add_vertex(2)
+    assert 1 in g.nodes()
+    assert 2 in g.nodes()
+
+def test_add_edge():
+    g = Graph()
+    g.add_vertex(1)
+    g.add_edge(3, 2)
+    assert len(g.nodes()) == 3
+    assert {3, 2} in g.edges()
