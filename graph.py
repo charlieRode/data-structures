9eb70177fff5e8264fe76ce7ed4a333980fb2639
Charlie Rode <chrode10@gmail.com>
2014-11-03 20:54:01 -0800

    Re-rename graph.py

--- graph.py
+++ graph.py
@@ -2,30 +2,30 @@
 
 
 class Edge(object):
-    def __init__(self, vertex1, vertex2):
+    def __init__(self, node1, node2):
         # For now, this is an unordered set since our edges have no direction
-        # and hence (v1, v2) will be counted the same as (v2, v1)
-        self.connects = set((vertex1, vertex2))
+        # and hence (n1, n2) will be counted the same as (n2, n1)
+        self.connects = set((node1, node2))
 
 
 class Graph(object):
     def __init__(self):
         self._edges = set()
-        self._verticies = set()
+        self._nodes = set()
 
     def nodes(self):
-        return self._verticies
+        return self._nodes
 
     def edges(self):
         return [edge.connects for edge in self._edges]
 
-    def add_vertex(self, data):
-        self._verticies.add(data)
+    def add_node(self, data):
+        self._nodes.add(data)
         return
 
     def add_edge(self, data1, data2):
-        self._verticies = self._verticies.union({data1, data2})
+        self._nodes.add(data1)
+        self._nodes.add(data2)
         edge = Edge(data1, data2)
         self._edges.add(edge)
         return
-
