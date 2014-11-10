8c5194964051a7964d1c3467e95b3089570e8f5a
Charlie Rode <chrode10@gmail.com>
2014-11-09 18:54:42 -0800

    Add __hash__() method to Node and Edge classes for comparison purposes. Add __repr__() function ot Edge class

--- new_graph.py
+++ new_graph.py
@@ -14,13 +14,30 @@ class Node(object):
     def __eq__(self, other):
         return self.data == other.data
 
+    def __hash__(self):
+        return hash(self.data)
+
 
 class Edge(object):
     # I'm storing the edge as a set of two data points,
-    # rather than two Node objects for simplicities sake.
+    # rather than two Node objects for simplicity's sake.
     # May need to change this later.
     def __init__(self, data1, data2):
+        self._d1 = data1
+        self._d2 = data2
         self.connects = set((data1, data2))
+
+    def __repr__(self):
+        return "Edge({data1}, {data2})".format(data1=self._d1, data2=self._d2)
+
+    def __str__(self):
+        return str(self.connects)
+
+    def __eq__(self, other):
+        return self.connects == other.connects
+
+    def __hash__(self):
+        return hash(self._d1) ^ hash(self._d2)
 
 
 class Graph(object):
@@ -44,12 +61,12 @@ class Graph(object):
         return [edge.connects for edge in self._edges]
 
     def delete_edge(self, data1, data2):
-        self._edges.remove()
+        edge = Edge(data1, data2)
+        if edge in self._edges:
+            self.edges.remove(edge)
 
     def delete_node(self, data):
         try:
             self._nodes.pop()
         except KeyError:
             raise IndexError('graph is empty')
-
-        
