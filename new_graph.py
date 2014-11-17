be1e60a3fa22ff078e8a3a6a36a4025533dc40a7
Charlie Rode <chrode10@gmail.com>
2014-11-16 17:47:30 -0800

    Make _get_node() a mothod global to the Graph class

--- new_graph.py
+++ new_graph.py
@@ -113,6 +113,11 @@ class Graph(object):
         else:
             return Edge(data1, data2) in self._edges
 
+    def _get_node(self, data):
+        for node in self._nodes:
+            if data == node.data:
+                return node
+
     def dfs(self, start):
         """Perfomes a depth-first-traversal, starting at Node(start)"""
         if self.has_node(start) is False:
@@ -124,11 +129,6 @@ class Graph(object):
             # Full traversal path to be returned by the method
         path = []
 
-        def _get_node(data):
-            for node in self._nodes:
-                if data == node.data:
-                    return node
-
         def _traverse(node):
             path.append(node.data)
             node.visited = True
@@ -136,7 +136,34 @@ class Graph(object):
                 if neighbor.visited is False:
                     _traverse(neighbor)
 
-        _traverse(_get_node(start))
+        _traverse(self._get_node(start))
+        for node in self._nodes:
+            node.visited = False
         return path
 
+        def bfs(self, start):
+            """Perfomes a breadth-first-traversal, starting at Node(start)"""
+            from queue import Queue
+            if self.has_node(start) is False:
+                raise IndexError("{start} not in graph".format(start=start))
+                return
+            for node in self._nodes:
+                # Assign each  node in the graph the attribute 'visited'
+                node.visited = False
+                # Full traversal path to be returned by the method
+            path = []
+            q = Queue()
+
+
+            def _get_node(data):
+                for node in self._nodes:
+                    if data == node.data:
+                        return node
+
+
+
+
+            _traverse(_get_node(start))
+            for node in self._nodes:
+                return path
 
